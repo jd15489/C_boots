7478d5508ef78cfcaec7310b652477f79438c2f6
Josh <joshdunn431@gmail.com>
2026-06-20 16:50:06 +0100

    Add rayon for parallel processing

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use std::io::Error;
 use rand::prelude::*;
 use csv::Reader;
 
+use rayon::prelude::*;
+
 const R: f64 = 0.00198720425864083;
-const N_SAMPLES: usize = 1000;
+const N_SAMPLES: usize = 10000;
 const N: usize = 900;
-const N_RESAMPLE: usize = 100000;
+const N_RESAMPLE: usize = 10000;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -22,22 +24,21 @@ fn main() -> std::io::Result<()> {
     let filename = &args[1];
     let temperature: &f64 = &args[2].parse().expect("Not a valid temperature");
 
-    let mut rng = rand::rng();
     let energies = read_file(filename);
-    let mut Cs: Vec<f64> = Vec::with_capacity(N_SAMPLES);
 
-    for i in 0..N_SAMPLES {
-        println!("\r{} / {}", i, N_SAMPLES);
-        let C = sample_and_compute_c(
-            &energies,
-            &mut rng,
-            temperature,
-        );
-        Cs.push(C);
-    }
+    let cs: Vec<f64> = (0..N_SAMPLES)
+        .into_par_iter()
+        .map(|_|{
+            let mut rng = rand::rng();
+            sample_and_compute_c(
+                &energies,
+                &mut rng,
+                *temperature)
+        })
+        .collect();
 
-    let average_c = average(&Cs);
-    let variance_c = variance(&Cs);
+    let average_c = average(&cs);
+    let variance_c = variance(&cs);
 
     println!("{} {}", average_c, variance_c);
     Ok(())
@@ -81,24 +82,22 @@ fn variance(vector: &Vec<f64>) -> f64 {
     variance
 }
 
-fn sample_and_compute_c(vector: &Vec<f64>, generator: &mut ThreadRng, temperature: &f64) -> f64 {
+fn sample_and_compute_c(vector: &Vec<f64>, generator: &mut ThreadRng, temperature: f64) -> f64 {
     let mut sum = 0.;
     let mut sq_sum = 0.;
 
     let mut n_resample = N_RESAMPLE;
-    if n_resample > vector.len() {
+    if N_RESAMPLE < 1 {
+        n_resample = vector.len();
+    } else if n_resample > vector.len() {
         n_resample = vector.len();
     }
 
-    for i in 0..n_resample {
-        if i % 10 == 0 {
-            print!("\r{} / {}", i, n_resample);
-        }
+    for _ in 0..n_resample {
         let index = generator.random_range(0..vector.len());
         sum += vector[index];
         sq_sum += vector[index].powf(2.);
     }
-    println!("");
 
     let average = sum / n_resample as f64;
     let sq_average = sq_sum / n_resample as f64;
