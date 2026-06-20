af5c70487aceee39ddcfdcb55c1eacc73b92d597
Josh <joshdunn431@gmail.com>
2026-06-20 19:49:42 +0100

    Add clap for command-line argument parsing and enhance main logic

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,66 @@
-use std::env;
-use std::io::Error;
-
 use rand::prelude::*;
 use csv::Reader;
 
 use rayon::prelude::*;
+use clap::Parser;
 
 const R: f64 = 0.00198720425864083;
-const N_SAMPLES: usize = 10000;
-const N: usize = 900;
-const N_RESAMPLE: usize = 10000;
+// const N_SAMPLES: usize = 10000;
+// const N: usize = 900;
+// const N_RESAMPLE: usize = 10000;
+
+#[derive(Parser, Debug)]
+#[command(
+    author="Josh Dunn",
+    version="0.1",
+    about="Computes heat capacity using a bootstrapping method."
+)]
+struct Args {
+    file: String,
+    temperature: f64,
+
+    #[arg(short, long, default_value_t = 1)]
+    molecules: usize,
+
+    #[arg(short, long, default_value_t = 10_000)]
+    num_samples: usize,
+
+    #[arg(short, long, default_value_t = 0)]
+    sample_size: usize,
+}
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 {
-        return Err(Error::new(
-            std::io::ErrorKind::InvalidInput,
-            "Usage: <program> <string>"
-        ));
+    let args = Args::parse();
+    
+    let energies = read_file(&args.file);
+    
+    let mut sample_size = args.sample_size;
+    if sample_size < 1 {
+        sample_size = energies.len();
+    } else if sample_size > energies.len() {
+        sample_size = energies.len();
     }
+    
+    
 
-    let filename = &args[1];
-    let temperature: &f64 = &args[2].parse().expect("Not a valid temperature");
-
-    let energies = read_file(filename);
-
-    let cs: Vec<f64> = (0..N_SAMPLES)
+    let cs: Vec<f64> = (0..args.num_samples)
         .into_par_iter()
         .map(|_|{
             let mut rng = rand::rng();
             sample_and_compute_c(
                 &energies,
                 &mut rng,
-                *temperature)
+                &args.temperature,
+                &sample_size,
+                &(args.molecules as f64))
         })
         .collect();
 
     let average_c = average(&cs);
     let variance_c = variance(&cs);
-
-    println!("{} {}", average_c, variance_c);
+    
+    println!("Average C | Standard Deviation in C");
+    println!(    "{:.7} | {:.7}", average_c, variance_c.sqrt());
     Ok(())
 }
 
@@ -50,14 +70,14 @@ fn read_file(filename: &str) -> Vec<f64> {
     let mut energies: Vec<f64> = Vec::new();
     let mut count = 0;
     for line in rdr.records() {
-        if count % 10 == 0 {
+        if count % 10_000 == 0 {
             print!("\rLine {} read", count);
         }
         count += 1;
         let record = line.unwrap()[0].parse().unwrap();
         energies.push(record);
     }
-    println!("\rFile Read");
+    println!("\rFile Read                ");
 
     energies
 }
@@ -82,26 +102,25 @@ fn variance(vector: &Vec<f64>) -> f64 {
     variance
 }
 
-fn sample_and_compute_c(vector: &Vec<f64>, generator: &mut ThreadRng, temperature: f64) -> f64 {
+fn sample_and_compute_c(
+    vector: &Vec<f64>,
+    generator: &mut ThreadRng,
+    temperature: &f64,
+    sample_size: &usize,
+    molecules: &f64
+) -> f64 {
     let mut sum = 0.;
     let mut sq_sum = 0.;
 
-    let mut n_resample = N_RESAMPLE;
-    if N_RESAMPLE < 1 {
-        n_resample = vector.len();
-    } else if n_resample > vector.len() {
-        n_resample = vector.len();
-    }
-
-    for _ in 0..n_resample {
+    for _ in 0..*sample_size {
         let index = generator.random_range(0..vector.len());
         sum += vector[index];
         sq_sum += vector[index].powf(2.);
     }
 
-    let average = sum / n_resample as f64;
-    let sq_average = sq_sum / n_resample as f64;
+    let average = sum / *sample_size as f64;
+    let sq_average = sq_sum / *sample_size as f64;
     let variance = sq_average - average.powf(2.);
 
-    variance / (R * (temperature * temperature)) / (N as f64)
+    variance / (R * (temperature * temperature)) / molecules
 }
