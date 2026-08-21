e04c00194f4d8c1ca66fa4f34e957d3c2c366439
Josh Dunn <71321756+jd15489@users.noreply.github.com>
2026-08-21 15:38:20 +0100

    Implement read_log function to parse log files

---
+++ Utils.py
@@ -0,0 +1,33 @@
+def read_log(file: Path) -> pd.DataFrame:
+    data = []
+    names = []
+    results = False
+    with file.open('r') as log_file:
+        for line in log_file:
+            if 'RESULTS' in line:
+                results = True
+                continue
+            elif 'A V E R A G E S' in line or 'TIMINGS' in line:
+                results = False
+                break
+
+            if results:
+                line = re.split(r"\[.*?\]|\(.*?\)|[\s=]+", line)
+                while '' in line:
+                    line.remove('')
+                if 'NSTEP' in line:
+                    data.append([])
+                    names.append([])
+                    data[-1].extend(line[1::2])
+                    names[-1].extend(line[::2])
+                    continue
+                elif 'Etot' in line:
+                    data[-1].extend(line[1::2])
+                    names[-1].extend(line[::2])
+                    continue
+                elif 'EKCMT' in line:
+                    data[-1].append(line[5])
+                    names[-1].append(line[4])
+                
+    data = pd.DataFrame(data, columns=names[0], dtype=float)
+    return data
